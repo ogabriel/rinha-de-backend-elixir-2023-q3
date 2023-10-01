fb6e9c525f3622f0375d9416139d387e2511ba87
ogabriel <gabrieloliver8991@gmail.com>
2023-10-01 16:27:45 -0300

    env

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :rinha, Rinha.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: System.get_env("DATABASE_HOSTNAME", "localhost"),
   database: "rinha_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
