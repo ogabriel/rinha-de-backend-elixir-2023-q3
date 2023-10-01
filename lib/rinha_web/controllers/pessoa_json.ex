eb48d90817e48601a0bcd5fe8bee08815ede8888
ogabriel <gabrieloliver8991@gmail.com>
2023-10-01 17:05:54 -0300

    update pessoa_json

--- lib/rinha_web/controllers/pessoa_json.ex
+++ lib/rinha_web/controllers/pessoa_json.ex
@@ -2,22 +2,10 @@ defmodule RinhaWeb.PessoaJSON do
   alias Rinha.Accounts.Pessoa
 
   @doc """
-  Renders a list of pessoas.
-  """
-  def index(%{pessoas: pessoas}) do
-    %{data: for(pessoa <- pessoas, do: data(pessoa))}
-  end
-
-  @doc """
   Renders a single pessoa.
   """
-  def show(%{pessoa: pessoa}) do
-    %{data: data(pessoa)}
-  end
-
-  defp data(%Pessoa{} = pessoa) do
+  def show(%{pessoa: %Pessoa{} = pessoa}) do
     %{
-      id: pessoa.id,
       apelido: pessoa.apelido,
       nome: pessoa.nome,
       nascimento: pessoa.nascimento,
