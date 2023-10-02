970d98fa998e35cfc4c9e6c87ba92eaba3193322
ogabriel <gabrieloliver8991@gmail.com>
2023-10-01 21:17:55 -0300

    upgrade docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-mix do ecto.create, ecto.migrate
-exec mix phx.server
+if [ "$1" = 'mix' ]; then
+    cd /app
+    mix do deps.get, deps.compile
+    mix do ecto.create, ecto.migrate
+    exec mix "$2"
+elif [ "$1" = 'release' ]; then
+    exec ./rel/elixir_app/bin/elixir_app start
+fi
