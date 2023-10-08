2071c80e8942c937aeec0ec6eb34b44d0b8c3783
ogabriel <gabrieloliver8991@gmail.com>
2023-10-08 09:39:03 -0300

    port

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  port = System.get_env("PORT") || "9999"
+  port = String.to_integer(System.get_env("PORT") || "9999")
 
   config :rinha, RinhaWeb.Endpoint,
     https: false,
