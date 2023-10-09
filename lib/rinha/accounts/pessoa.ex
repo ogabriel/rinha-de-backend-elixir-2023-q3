cc235ea4e7930bca6edcf538558f2fbadcd0b8fa
ogabriel <gabrieloliver8991@gmail.com>
2023-10-09 08:52:46 -0300

    add more validations

--- lib/rinha/accounts/pessoa.ex
+++ lib/rinha/accounts/pessoa.ex
@@ -5,10 +5,10 @@ defmodule Rinha.Accounts.Pessoa do
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "pessoas" do
-    field :stack, {:array, :string}
     field :apelido, :string
     field :nome, :string
     field :nascimento, :date
+    field :stack, {:array, :string}
 
     timestamps()
   end
@@ -18,6 +18,19 @@ defmodule Rinha.Accounts.Pessoa do
     pessoa
     |> cast(attrs, [:apelido, :nome, :nascimento, :stack])
     |> validate_required([:apelido, :nome, :nascimento])
+    |> validate_length(:apelido, count: :bytes, min: 1, max: 32)
+    |> validate_length(:nome, count: :bytes, min: 1, max: 32)
+    |> validate_stack
     |> unique_constraint(:apelido)
+  end
+
+  defp validate_stack(changeset) do
+    with stack <- get_field(changeset, :stack),
+         true <- is_list(stack),
+         false <- Enum.all?(stack, &(is_binary(&1) && byte_size(&1) in 1..32)) do
+      add_error(changeset, :stack, "must be a list of strings")
+    else
+      _ -> changeset
+    end
   end
 end
