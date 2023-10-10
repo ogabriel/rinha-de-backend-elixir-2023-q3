e3275ff67934212d44d5576628074a0f0122ed02
ogabriel <gabrieloliver8991@gmail.com>
2023-10-10 09:38:30 -0300

    better validations

--- lib/rinha/accounts/pessoa.ex
+++ lib/rinha/accounts/pessoa.ex
@@ -20,15 +20,27 @@ defmodule Rinha.Accounts.Pessoa do
     |> validate_required([:apelido, :nome, :nascimento])
     |> validate_length(:apelido, count: :bytes, min: 1, max: 32)
     |> validate_length(:nome, count: :bytes, min: 1, max: 100)
-    |> validate_stack
+    |> validate_apelido_with_cache
     |> unique_constraint(:apelido)
+    |> validate_stack
+  end
+
+  defp validate_apelido_with_cache(changeset) do
+    with true <- Enum.empty?(changeset.errors),
+         apelido <- get_field(changeset, :apelido),
+         true <- Cachex.get!(:pessoas_apelido, apelido) do
+      add_error(changeset, :nome, "must be a valid string")
+    else
+      _ -> changeset
+    end
   end
 
   defp validate_stack(changeset) do
-    with stack <- get_field(changeset, :stack),
+    with true <- Enum.empty?(changeset.errors),
+         stack <- get_field(changeset, :stack),
          true <- is_list(stack),
          false <- Enum.all?(stack, &(is_binary(&1) && byte_size(&1) in 1..32)) do
-      add_error(changeset, :stack, "must be a list of strings")
+      add_error(changeset, :stack, "is invalid", type: :string, validation: :cast)
     else
       _ -> changeset
     end
