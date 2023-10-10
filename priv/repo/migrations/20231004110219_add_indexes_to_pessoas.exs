1de66e0c06a1ad35650b0259a0b92bca330be686
ogabriel <gabrieloliver8991@gmail.com>
2023-10-10 20:37:22 -0300

    update _add_indexes_to_pessoas

--- priv/repo/migrations/20231004110219_add_indexes_to_pessoas.exs
+++ priv/repo/migrations/20231004110219_add_indexes_to_pessoas.exs
@@ -3,5 +3,8 @@ defmodule Rinha.Repo.Migrations.AddIndexesToPessoas do
 
   def change do
     create unique_index(:pessoas, [:apelido])
+
+    execute("CREATE EXTENSION pg_trgm")
+    create index(:pessoas, ["busca gist_trgm_ops"], using: :gist)
   end
 end
