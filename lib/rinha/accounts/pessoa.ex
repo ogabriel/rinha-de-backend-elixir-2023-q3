f0d2165e4fac1a5792586c19b429d5351819e503
ogabriel <gabrieloliver8991@gmail.com>
2023-11-02 11:12:14 -0300

    feat: move all validations to controller

--- lib/rinha/accounts/pessoa.ex
+++ lib/rinha/accounts/pessoa.ex
@@ -2,6 +2,8 @@ defmodule Rinha.Accounts.Pessoa do
   use Ecto.Schema
   import Ecto.Changeset
 
+  require Logger
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "pessoas" do
@@ -17,38 +19,10 @@ defmodule Rinha.Accounts.Pessoa do
   @doc false
   def changeset(pessoa, attrs) do
     pessoa
-    |> cast(attrs, [:apelido, :nome, :nascimento, :stack])
-    |> validate_required([:apelido, :nome, :nascimento])
-    |> validate_length(:apelido, count: :bytes, min: 1, max: 32)
-    |> validate_length(:nome, count: :bytes, min: 1, max: 100)
-    |> validate_nascimento
+    |> cast(attrs, [:apelido, :nome, :nascimento, :stack], empty_values: [])
     |> unique_constraint(:apelido)
-    |> validate_stack
     |> build_busca
   end
-
-  defp validate_nascimento(%{errors: []} = changeset) do
-    with nascimento <- get_field(changeset, :nascimento),
-         {:ok, _} <- Date.from_iso8601(nascimento) do
-      changeset
-    else
-      _ -> add_error(changeset, :nascimento, "is invalid", type: :date, validation: :cast)
-    end
-  end
-
-  defp validate_nascimento(changeset), do: changeset
-
-  defp validate_stack(%{errors: []} = changeset) do
-    with stack <- get_field(changeset, :stack),
-         true <- is_list(stack),
-         false <- Enum.all?(stack, &(is_binary(&1) && byte_size(&1) in 1..32)) do
-      add_error(changeset, :stack, "is invalid", type: :string, validation: :cast)
-    else
-      _ -> changeset
-    end
-  end
-
-  defp validate_stack(changeset), do: changeset
 
   defp build_busca(%{errors: []} = changeset) do
     busca =
--- lib/rinha_web/controllers/pessoa_controller.ex
+++ lib/rinha_web/controllers/pessoa_controller.ex
@@ -5,23 +5,60 @@ defmodule RinhaWeb.PessoaController do
   alias Rinha.Accounts.Pessoa
 
   def create(conn, pessoa_params) do
-    with {:ok, %Pessoa{} = pessoa} <- Accounts.create_pessoa(pessoa_params) do
-      conn
-      |> put_status(201)
-      |> put_resp_header("location", ~p"/pessoas/#{pessoa}")
-      |> text("")
-    else
-      {:error, changeset} ->
-        if non_wrong_type?(changeset) do
-          send_resp(conn, 422, "")
+    cond do
+      missing_params?(pessoa_params) ->
+        send_resp(conn, 422, "")
+
+      incorrect_types?(pessoa_params) ->
+        send_resp(conn, 400, "")
+
+      incorrect_sizes?(pessoa_params) ->
+        send_resp(conn, 422, "")
+
+      incorrect_stack?(pessoa_params) ->
+        send_resp(conn, 400, "")
+
+      true ->
+        with {:ok, %Pessoa{} = pessoa} <- Accounts.create_pessoa(pessoa_params) do
+          conn
+          |> put_status(201)
+          |> put_resp_header("location", ~p"/pessoas/#{pessoa}")
+          |> text("")
         else
-          send_resp(conn, 400, "")
+          _ ->
+            send_resp(conn, 422, "")
         end
     end
   end
 
-  defp non_wrong_type?(%{errors: [{_, {_, error_data}} | _]}) do
-    error_data[:validation] not in [:cast]
+  defp missing_params?(%{"apelido" => apelido, "nome" => nome, "nascimento" => nascimento}) do
+    is_nil(apelido) || is_nil(nome) || is_nil(nascimento)
+  end
+
+  defp incorrect_types?(%{"apelido" => apelido, "nome" => nome, "nascimento" => nascimento}) do
+    not (is_binary(apelido) && is_binary(nome) && valid_date?(nascimento))
+  end
+
+  defp valid_date?(nascimento) do
+    with true <- is_binary(nascimento),
+         true <- byte_size(nascimento) == 10,
+         {:ok, _} <- Date.from_iso8601(nascimento) do
+      true
+    else
+      _ -> false
+    end
+  end
+
+  defp incorrect_sizes?(%{"apelido" => apelido, "nome" => nome}) do
+    not (byte_size(apelido) in 1..32 &&
+           byte_size(nome) in 1..100)
+  end
+
+  defp incorrect_stack?(%{"stack" => nil}), do: false
+
+  defp incorrect_stack?(%{"stack" => stack}) do
+    not (is_list(stack) &&
+           Enum.all?(stack, fn x -> is_binary(x) && byte_size(x) in 1..32 end))
   end
 
   def show(conn, %{"id" => id}) do
